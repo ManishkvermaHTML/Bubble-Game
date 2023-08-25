4b5a29859ed5a866836f8d51382bdc9bdeac0b48
Manish kumar <manish.html5.0@gmail.com>
2023-08-25 21:21:51 +0530

    hit and timer function

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
+function createBubble(){
 let createBubble = "";
-for(let i = 0; i<=98; i++){
-   createBubble += `<div class="bubble">5</div>`;
+for(let i = 0; i<=108; i++){
+   let randNum = Number(Math.floor((0 + (10-0))*Math.random()));
+   createBubble += `<div class="bubble">${randNum}</div>`;
 }
 document.querySelector("#bottomPanel").innerHTML = createBubble;    
+}
+
+function hitNumber(){
+   let randNum = Number(Math.floor((0 + (10-0))*Math.random()));
+   document.querySelector("#hitNumber").innerHTML = randNum;
+}
+
+function timer(){
+   var time = 60;
+   let timer = setInterval(()=>{
+      if(time > 0){
+         time--;
+         document.getElementById("timer").innerHTML = time;
+      }
+      else{
+         clearInterval(timer);
+         console.log("end intervel");
+      }
+   },1000)
+}
+
+createBubble();
+hitNumber();
+timer();
