92dc70e58351d7a7299531225d283c4fef1334c0
Manish kumar <manish.html5.0@gmail.com>
2023-08-27 15:18:27 +0530

    score function complete

--- index.js
+++ index.js
@@ -1,31 +1,56 @@
-function createBubble(){
-let createBubble = "";
-for(let i = 0; i<=108; i++){
-   let randNum = Number(Math.floor((0 + (10-0))*Math.random()));
-   createBubble += `<div class="bubble">${randNum}</div>`;
-}
-document.querySelector("#bottomPanel").innerHTML = createBubble;    
+var randNum;
+var scoreValue = 0;
+function createBubble() {
+   let createBubble = "";
+   for (let i = 0; i <= 108; i++) {
+      let randNum = Number(Math.floor((0 + (10 - 0)) * Math.random()));
+      createBubble += `<div class="bubble">${randNum}</div>`;
+   }
+   document.querySelector("#bottomPanel").innerHTML = createBubble;
 }
 
-function hitNumber(){
-   let randNum = Number(Math.floor((0 + (10-0))*Math.random()));
+function hitNumberGenerator() {
+   randNum = Number(Math.floor((0 + (10 - 0)) * Math.random()));
    document.querySelector("#hitNumber").innerHTML = randNum;
 }
 
-function timer(){
+function timer() {
    var time = 60;
-   let timer = setInterval(()=>{
-      if(time > 0){
+   let timer = setInterval(() => {
+      if (time > 0) {
          time--;
          document.getElementById("timer").innerHTML = time;
       }
-      else{
+      else {
          clearInterval(timer);
          console.log("end intervel");
       }
-   },1000)
+   }, 1000)
 }
 
+function score(cValue) {
+   let bubble = document.getElementById("bottomPanel");
+   bubble.addEventListener("click", (e) => {
+      // console.log(e);
+      let uValue = Number(e.target.innerText);
+      let isSameValue = cValue == uValue;
+      if (isSameValue) {
+         scoreValue++;
+         document.getElementById("scoreBox").innerHTML = scoreValue;
+         hitNumberGenerator();
+         createBubble();
+         score(randNum);
+         // timer();
+         // console.log("inside if",scoreValue);
+      }
+      // else{
+      //    // console.log("wrong number",e);
+      //    e.target.style.color = "red";
+      // }
+
+   });
+}
 createBubble();
-hitNumber();
+hitNumberGenerator();
 timer();
+score(randNum);
